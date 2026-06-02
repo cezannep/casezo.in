40dbc5fd5c25bec66e3af1785666f9dcf451c3fa
cezannep <cezan.git@gmail.com>
2026-06-02 12:19:23 +0530

    chore: initialize Casezo.in project

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <div className="flex flex-col justify-center items-center w-screen h-screen">
+      <h1 className="text-7xl mb-5">Casezo.in</h1> 
+      <h1>The future statrs now.</h1>
+    </div>
+   
+  );
+}
