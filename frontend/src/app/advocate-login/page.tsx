58299d0c0c576c27d663d10adc3b8e898ac7959c
cezannep <cezan.git@gmail.com>
2026-06-09 12:14:41 +0530

    feat(advocate-login): add advocate login with validation

---
+++ frontend/src/app/advocate-login/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import AdvocateLogin from '../components/auth/AdvocateLogin'
+
+const page = () => {
+  return (
+    <AdvocateLogin/>
+  )
+}
+
+export default page
---
+++ frontend/src/app/schemas/auth.advocateLoginSchema.ts
@@ -0,0 +1,18 @@
+import { z } from "zod";
+
+export const advocateLoginSchema = z.object({
+  email: z.email("Please enter a valid email address"),
+  password: z
+    .string()
+    .min(8, "Password must be at least 8 characters")
+    .max(32, "Password cannot exceed 32 characters")
+    .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
+    .regex(/[a-z]/, "Password must contain at least one lowercase letter")
+    .regex(/[0-9]/, "Password must contain at least one number")
+    .regex(
+      /[!@#$%^&*(),.?":{}|<>]/,
+      "Password must contain at least one special character",
+    ),
+});
+
+export type AdvocateLoginFormData = z.infer<typeof advocateLoginSchema>;
