16684a1312166adad96c0f0ad80c18e30e1d885b
cezannep <cezan.git@gmail.com>
2026-06-09 11:33:52 +0530

    feat(admin-login): add admin login page with validation

---
+++ frontend/src/app/components/auth/AdminLogin.tsx
@@ -0,0 +1,75 @@
+"use client";
+import Link from "next/link";
+import Input from "../ui/Input";
+import Button from "../ui/Button";
+import { useAdminLogin } from "@/app/hooks/useAdminLogin";
+
+export default function AdminLogin() {
+  const { form, onSubmit } = useAdminLogin();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = form;
+
+  return (
+    <section className="min-h-screen flex">
+      {/* Left Side */}
+
+      <div className="w-full lg:w-[42%] bg-black text-white flex items-center justify-center px-8">
+        <div className="w-full max-w-sm">
+          {/* Header */}
+          <div className="text-center">
+            <Link href={"/"}>
+              <h1 className=" lg:hidden text-5xl font-black leading-none">
+                casezo
+                <span className="text-[#A78344]">.in</span>
+              </h1>
+            </Link>
+
+            <h2 className="mt-5 lg:mt-12 text-2xl lg:text-5xl font-serif ">
+              Admin Login
+            </h2>
+          </div>
+
+          {/* Form */}
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="mt-5 lg:mt-10 space-y-4"
+            noValidate
+          >
+            <Input
+              type="email"
+              placeholder="Email"
+              className="outline-none focus:border-[#A78344]"
+              error={errors.email?.message}
+              {...register("email")}
+            />
+
+            <Input
+              type="password"
+              placeholder="Password"
+              className="outline-none focus:border-[#A78344]"
+              error={errors.password?.message}
+              {...register("password")}
+            />
+
+            <Button className="mt-3" type="submit">
+              LOGIN
+            </Button>
+          </form>
+        </div>
+      </div>
+
+      {/* Right Side */}
+      <div className="hidden lg:flex flex-1 bg-[#f5f5f5] items-center justify-center">
+        <Link href={"/"}>
+          <h1 className="text-[120px] font-black leading-none">
+            casezo
+            <span className="text-[#A78344]">.in</span>
+          </h1>
+        </Link>
+      </div>
+    </section>
+  );
+}
